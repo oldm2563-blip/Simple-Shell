d65543a43a757a86891a26b64fdd1783570dcf6b
oldm2563-blip <oldm2563@gmail.com>
2026-09-09 15:53:11 +0100

    make the start in the Main class and make the signup and login

--- src/main/java/ma/youcode/lineperma/Service/UserService.java
+++ src/main/java/ma/youcode/lineperma/Service/UserService.java
@@ -48,3 +48,52 @@ public class UserService {
         return userMap;
     }
 
+    public void CreateUser(){
+        Map<String, User> userMap = loadUsersToMap();
+        String filePath = "C:\\java-bootcamp\\simple-shell\\src\\main\\resources\\Users.txt ";
+        
+        System.out.print("Enter username: ");
+        String name = scanner.nextLine().trim();
+
+        if (userMap.containsKey(name)) {
+            System.out.println("Error: Username '" + name + "' is already taken!");
+            return;
+        }
+
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine().trim();
+
+        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt(12));
+
+
+
+        try (FileWriter writer = new FileWriter(filePath, true)) {
+                writer.write(name + ":" + hashedPassword + System.lineSeparator());
+                System.out.println("User registered successfully.");
+            }
+             catch (IOException e) {
+            System.out.println("Could not write to file: " + e.getMessage());
+        }
+    
+}
+
+    public User login(){
+        Map<String, User> userMap = loadUsersToMap();
+        System.out.print("Enter username: ");
+        String name = scanner.nextLine().trim();
+
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine().trim();
+
+        if(userMap.containsKey(name)){
+            User existingUser = userMap.get(name);
+            if(BCrypt.checkpw(password, existingUser.getPassword())){
+                System.out.println("user Logged in");
+                return existingUser;
+            }
+           System.out.println("User info wrong.");
+            return null;
+        }System.out.println("User info wrong.");
+        return null;
+}
+}
