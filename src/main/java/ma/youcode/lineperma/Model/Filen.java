712cdc478c99f3d5e05d04a9e3fff1107a74bce5
oldm2563-blip <oldm2563@gmail.com>
2026-09-11 17:39:28 +0100

    constructor

--- src/main/java/ma/youcode/lineperma/Model/Filen.java
+++ src/main/java/ma/youcode/lineperma/Model/Filen.java
@@ -4,4 +4,11 @@ public class Filen {
     private String file;
     private String username;
     private String[] permission = {"-", "-", "-"};
+
+    public Filen(String file, String username, String[] permission) {
+        this.file = file;
+        this.username = username;
+        this.permission = permission;
+    }
+
 }
