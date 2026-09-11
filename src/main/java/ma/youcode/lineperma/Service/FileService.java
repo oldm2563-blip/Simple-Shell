9bdaf290dbff4c1ede387bbe9104732f7aedeeec
oldm2563-blip <oldm2563@gmail.com>
2026-09-11 17:41:00 +0100

    startup

--- src/main/java/ma/youcode/lineperma/Service/FileService.java
+++ src/main/java/ma/youcode/lineperma/Service/FileService.java
@@ -1,3 +1,20 @@
 package ma.youcode.lineperma.Service;
 public class FileService {
+    public void starup(){
+        if (!Files.exists(path2)) {
+             try (FileWriter writer = new FileWriter(path2.toFile(), true)) {
+            } catch (IOException e) {
+                System.out.println("Could not create/write file");
+            }
+        }
+
+        try{
+            List<String> lines = Files.readAllLines(path2);
+            for(String line : lines){
+                String[] parts = line.split(":", 3);
+                String fileName = parts[0];
+                String userName = parts[1];
+                String[] permission = parts[2].split("");
+                fileMap.put(fileName, new Filen(fileName, userName, permission));
+            }
 }
